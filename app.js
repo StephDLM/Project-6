dde3aab2f744d1d75f71c231cd188214903e1aeb
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-12 22:28:57 -0700

    setting up new repo

---
+++ app.js
@@ -0,0 +1,59 @@
+//variables 
+const express = require('express');
+const app = express();
+var data = require('data.json');
+
+//middleware
+    // view engine setup
+app.set('view engine', 'pug');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use('/', indexRouter);
+
+//static route to serve static files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//set routes
+app.get('/', (req, res) => {
+    res.render("index", {data});
+});
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+//dynamic project route 
+app.get('/project.id', (req, res) => {
+    const dataProject = data[req.params.id]
+    if (dataProject) {
+      res.render('project', { dataProject });
+    } else if ( dataProject === undefined ){
+        const err = new Error('Not Found');
+        err.status = 404;
+        err.message = "This web page can't be located"
+        res.render('error', { error: err })
+    }});
+
+//render error page 404
+app.use ((req,res) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = "This web page can't be located";
+    res.render('error', { error: err })
+
+})
+
+app.use((err, req, res, next) => {
+    if (err === 404){
+        next(err)
+    } else {
+        err.status = 500;
+        err.message = "Server isn't connected";
+        res.render('error', { error: err })
+    }
+  });
+
+//start server
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
