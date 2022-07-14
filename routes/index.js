953cc2dd258ef777874890c1f70291f9af3b63f5
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-13 21:57:19 -0700

    updating routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-var data = require('../data.json')
+var data = require('../data/data.json')
 const app = express();
 
 //middleware
